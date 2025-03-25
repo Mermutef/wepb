d5c7af61339dc438a54e1c191cae0930ed5c9b6a
EgorKolupaev0502 <kolupaew.eg@yangex.ru>
2025-03-25 16:40:57 +0300

    Regenerate DB schema and downgrade some dependencies (refs #10)

--- src/main/resources/migrations/V01__create_users.sql
+++ src/main/resources/migrations/V01__create_users.sql
@@ -1,6 +1,8 @@
 CREATE TYPE user_role AS ENUM (
     'ADMIN',
     'MODERATOR',
+    'WRITER',
+    'READER',
     'AUTHORIZED'
 );
 
@@ -11,6 +13,5 @@ CREATE TABLE users (
 	name CHARACTER VARYING(30) UNIQUE NOT NULL,
 	email VARCHAR(255) UNIQUE NOT NULL,
     password CHARACTER(64) NOT NULL,
-    role user_role NOT NULL,
-    CHECK (email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$')
+    role user_role NOT NULL
 );
