45942f8822aa90895eb51255b13834c34ab19a39
egorkokupaev <kolupaew.eg@yandex.ru>
2025-04-05 18:46:25 +0300

    Изменены разрешения у ролей (refs #10)

--- src/main/kotlin/ru/yarsu/domain/accounts/Role.kt
+++ src/main/kotlin/ru/yarsu/domain/accounts/Role.kt
@@ -2,96 +2,41 @@ package ru.yarsu.domain.accounts
 
 @Suppress("detekt:LongParameterList")
 enum class Role(
-    val attachMediaFile: Boolean = false, // Прикреплять имеющиеся в приложении медиафайлы к постам
-    val deleteYourPost: Boolean = false, // Удалять свои черновики постов
-    val editAllPosts: Boolean = false, // Редактировать посты (свои и писателей)
-    val editOrDeleteYourComment: Boolean = false, // Редактировать и удалить свой комментарий
-    val editYourPost: Boolean = false, // Редактировать свои неопубликованные посты
-    val filterData: Boolean = false, // Фильтровать по хештегам, дате публикации и последнего редактирования
-    val hideCommentOtherUser: Boolean = false, // Скрывать комментарии под постами
-    val hidePost: Boolean = false, // скрывать посты (свои и писателей)
-    val publishNewPost: Boolean = false, // Публиковать новые посты
-    val publishWritersPost: Boolean = false, // Публиковать посты писателей
-    val recallPostForModeration: Boolean = false, // Отзывать посты с модерации
-    val saveDraftPost: Boolean = false, // Сохранять черновики постов
-    val sendPostForModeration: Boolean = false, // Отправлять посты на модерацию
-    val viewAllMediaFile: Boolean = false, // Просматривать все имеющиеся в приложении медиафайлы
+    val writeEditDeleteComment: Boolean = false, // Оставить, изменять, удалять комментарий
     val viewInformationBureau: Boolean = false, // Просматривать публичную информацию о профбюро
+    val manageYourPost: Boolean = false, // управлять своими постами
+    val manageMediaFile: Boolean = false, // Загружать, просматривать, прикреплять медиафайлы
+    val manageAllPosts: Boolean = false, // редактировать, публиковать, скрывать посты (свои и писателей)
+    val hideCommentOtherUser: Boolean = false, // Скрывать комментарии под постами
     val viewListUser: Boolean = false, // Просматривать список пользователей в системе, их роли
-    val viewMedia: Boolean = false, // Просмотр постов, комментариев к постам, профилей модераторов
-    val writeComment: Boolean = false, // Оставить комментарий
     val updateRole: Boolean = false, // выдавать и снимать полномочия модераторов и писателей
-    val uploadMediaFile: Boolean = false, // Загружать медиафайлы (изображения, аудиофайлы, видео)
 ) {
-    ANONYMOUS(
-        viewMedia = true,
-        filterData = true
-    ),
+    ANONYMOUS(),
     READER(
-        viewMedia = true,
-        filterData = true,
-        writeComment = true,
-        editOrDeleteYourComment = true,
+        writeEditDeleteComment = true,
         viewInformationBureau = true
     ),
     WRITER(
-        viewMedia = true,
-        filterData = true,
-        writeComment = true,
-        editOrDeleteYourComment = true,
+        writeEditDeleteComment = true,
         viewInformationBureau = true,
-        saveDraftPost = true,
-        sendPostForModeration = true,
-        recallPostForModeration = true,
-        editYourPost = true,
-        deleteYourPost = true,
-        uploadMediaFile = true,
-        viewAllMediaFile = true,
-        attachMediaFile = true
+        manageYourPost = true,
+        manageMediaFile = true,
     ),
     MODERATOR(
-        viewMedia = true,
-        filterData = true,
-        writeComment = true,
-        editOrDeleteYourComment = true,
+        writeEditDeleteComment = true,
         viewInformationBureau = true,
-        saveDraftPost = true,
-//        ?
-        sendPostForModeration = true,
-        recallPostForModeration = true,
-//        ?
-        editYourPost = true,
-        deleteYourPost = true,
-        uploadMediaFile = true,
-        viewAllMediaFile = true,
-        attachMediaFile = true,
+        manageYourPost = true,
+        manageMediaFile = true,
         hideCommentOtherUser = true,
-        publishWritersPost = true,
-        publishNewPost = true,
-        editAllPosts = true,
-        hidePost = true
+        manageAllPosts = true,
     ),
     ADMIN(
-        viewMedia = true,
-        filterData = true,
-        writeComment = true,
-        editOrDeleteYourComment = true,
+        writeEditDeleteComment = true,
         viewInformationBureau = true,
-        saveDraftPost = true,
-//       ?
-        sendPostForModeration = true,
-        recallPostForModeration = true,
-//       ?
-        editYourPost = true,
-        deleteYourPost = true,
-        uploadMediaFile = true,
-        viewAllMediaFile = true,
-        attachMediaFile = true,
+        manageYourPost = true,
+        manageMediaFile = true,
         hideCommentOtherUser = true,
-        publishWritersPost = true,
-        publishNewPost = true,
-        editAllPosts = true,
-        hidePost = true,
+        manageAllPosts = true,
         viewListUser = true,
         updateRole = true
     ),
