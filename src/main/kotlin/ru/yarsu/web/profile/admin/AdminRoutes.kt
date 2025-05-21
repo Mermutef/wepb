22188eb8f7a4ce3156de08464915f3eb7cbf8e98
Drozd Elena <dr0zd.len@yandex.ru>
2025-05-21 23:04:50 +0300

    Initial (refs #27)

--- src/main/kotlin/ru/yarsu/web/profile/admin/AdminRoutes.kt
+++ src/main/kotlin/ru/yarsu/web/profile/admin/AdminRoutes.kt
@@ -1,19 +1,27 @@
 package ru.yarsu.web.profile.admin
-import dev.forkhandles.result4k.Failure
-import dev.forkhandles.result4k.Result
-import dev.forkhandles.result4k.Success
-import org.http4k.core.*
-import org.http4k.lens.RequestContextLens
-import ru.yarsu.db.posts.PostsOperations
-import ru.yarsu.domain.accounts.Role
-import ru.yarsu.domain.models.Post
-import ru.yarsu.domain.models.User
-import ru.yarsu.domain.operations.posts.PostFetchingError
-import ru.yarsu.domain.operations.posts.PostsOperationsHolder
-import ru.yarsu.domain.operations.users.UserOperationsHolder
-import ru.yarsu.web.auth.lenses.UserWebLenses.authorizeUserFromPath
-import ru.yarsu.web.context.templates.ContextAwareViewRender
-import ru.yarsu.web.extract
-import ru.yarsu.web.notFound
-import ru.yarsu.web.profile.writer.models.AdminRoomVM
 
+import org.http4k.core.Method
+import org.http4k.routing.RoutingHttpHandler
+import org.http4k.routing.bind
+import org.http4k.routing.routes
+import ru.yarsu.db.users.UserOperations
+import ru.yarsu.domain.operations.OperationsHolder
+import ru.yarsu.domain.operations.users.UserOperationsHolder
+import ru.yarsu.web.auth.lenses.UserWebLenses
+import ru.yarsu.web.context.ContextTools
+import ru.yarsu.web.profile.admin.handlers.AdminHandler
+
+fun adminRoutes(
+    contextTools: ContextTools,
+    operations: UserOperationsHolder,
+) = routes(
+    "$ADMIN_SEGMENT$LIST_ALL_USERS" bind Method.GET to AdminHandler(
+        render = contextTools.render,
+        userOperations = operations,
+        userLens = contextTools.userLens,
+    )
+)
+
+// Константы для путей
+const val ADMIN_SEGMENT = "/admin"
+const val LIST_ALL_USERS = "/users"
