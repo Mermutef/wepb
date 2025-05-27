45845eb02614ef57592335a2b0a908130b83b23e
Anya Aladina <anka.aladjina@yandex.ru>
2025-05-27 14:28:55 +0300

    Ktlint and detect(refs #28)

--- src/main/kotlin/ru/yarsu/web/posts/PostsRoutes.kt
+++ src/main/kotlin/ru/yarsu/web/posts/PostsRoutes.kt
@@ -2,18 +2,15 @@ package ru.yarsu.web.posts
 
 import org.http4k.core.*
 import org.http4k.routing.*
-import ru.yarsu.domain.operations.OperationsHolder
 import ru.yarsu.web.context.ContextTools
 import ru.yarsu.web.posts.handlers.ShowNewPostFormHandler
 
-fun postsRoutes(
-    contextTools: ContextTools,
-    operations: OperationsHolder,
-) = routes(
-    CREATE_POST bind Method.GET to ShowNewPostFormHandler(
-        contextTools.render,
-    ),
-)
+fun postsRoutes(contextTools: ContextTools) =
+    routes(
+        CREATE_POST bind Method.GET to ShowNewPostFormHandler(
+            contextTools.render,
+        ),
+    )
 
 const val POST_SEGMENT = "/posts"
 const val CREATE_POST = "/new-post"
