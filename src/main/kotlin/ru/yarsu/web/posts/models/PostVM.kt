5b351e6ca2d72ae1b78696d75287101240e59710
Anya Aladina <anka.aladjina@yandex.ru>
2025-06-06 12:21:44 +0300

    Added post page(refs #44)
    # Conflicts:
    #	src/main/kotlin/ru/yarsu/web/posts/models/PostVM.kt
    #	src/main/resources/ru/yarsu/web/posts/models/PostVM.peb

--- src/main/kotlin/ru/yarsu/web/posts/models/PostVM.kt
+++ src/main/kotlin/ru/yarsu/web/posts/models/PostVM.kt
@@ -4,11 +4,11 @@ import org.http4k.template.ViewModel
 import ru.yarsu.domain.models.Hashtag
 import ru.yarsu.domain.models.Post
 import ru.yarsu.domain.models.User
-import ru.yarsu.web.posts.utils.render
+import ru.yarsu.web.rendering.MarkdownToHTMLRenderer
 
 class PostVM(
     val postWithHashtag: Pair<Post, Hashtag>,
     val author: User,
 ) : ViewModel {
-    fun renderMD(content: String) = content.render()
+    fun renderMD(content: String) = MarkdownToHTMLRenderer.render(content)
 }
