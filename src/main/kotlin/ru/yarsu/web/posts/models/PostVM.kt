3fe054f5be63a155b900b55f47c2a9f69b5ac168
Vladislav Kopnin <vlad.kopnen@mail.ru>
2025-06-09 09:35:33 +0300

    Media rendering (resolves #44)

--- src/main/kotlin/ru/yarsu/web/posts/models/PostVM.kt
+++ src/main/kotlin/ru/yarsu/web/posts/models/PostVM.kt
@@ -4,11 +4,11 @@ import org.http4k.template.ViewModel
 import ru.yarsu.domain.models.Hashtag
 import ru.yarsu.domain.models.Post
 import ru.yarsu.domain.models.User
-import ru.yarsu.web.rendering.MarkdownToHTMLRenderer
+import ru.yarsu.web.posts.utils.render
 
 class PostVM(
     val postWithHashtag: Pair<Post, Hashtag>,
     val author: User,
 ) : ViewModel {
-    fun renderMD(content: String) = MarkdownToHTMLRenderer.render(content)
+    fun renderMD(content: String) = content.render()
 }
