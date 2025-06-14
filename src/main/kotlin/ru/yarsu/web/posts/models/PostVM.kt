4cee4e46ff592bf0485fc580df44a938acf111d5
Vladislav <114745176+Mermutef@users.noreply.github.com>
2025-06-14 12:25:37 +0300

    Bugfix: unauthorized users can't read comments (resolves #55)
    * Fix (resolves #55)
    
    * ktlint (resolves #55)
    
    * fix (resolves #55)
    
    * fix styles (resolves #55)

--- src/main/kotlin/ru/yarsu/web/posts/models/PostVM.kt
+++ src/main/kotlin/ru/yarsu/web/posts/models/PostVM.kt
@@ -4,11 +4,17 @@ import org.http4k.template.ViewModel
 import ru.yarsu.domain.models.Hashtag
 import ru.yarsu.domain.models.Post
 import ru.yarsu.domain.models.User
+import ru.yarsu.web.auth.AUTH_SEGMENT
+import ru.yarsu.web.auth.SIGN_IN
+import ru.yarsu.web.auth.SIGN_UP
 import ru.yarsu.web.posts.utils.render
 
 class PostVM(
     val postWithHashtag: Pair<Post, Hashtag>,
     val author: User,
 ) : ViewModel {
+    val signInLink = "$AUTH_SEGMENT$SIGN_IN"
+    val signUpLink = "$AUTH_SEGMENT$SIGN_UP"
+
     fun renderMD(content: String) = content.render()
 }
